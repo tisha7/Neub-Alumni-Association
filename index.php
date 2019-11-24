720d6a55946b7c4092f8fcc1189033e3474eca82
Ishrat Jahan Tisha <47575896+tisha7@users.noreply.github.com>
2019-11-25 03:52:04 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+    require_once("functions/function.php");
+    get_header();
+?>
+    <!--navigation>-->
+    <div class="navbar" >
+       <a href="index.php" class="active">Home</a>
+       <a href="#news">News</a>
+     <div class="dropdown">
+   <button class="dropbtn">Committee
+     <i class="fa fa-caret-down"></i>
+   </button>
+   <div class="dropdown-content">
+     <a href="#">Link 1</a>
+     <a href="#">Link 2</a>
+     <a href="#">Link 3</a>
+   </div>
+   </div>
+   <div class="dropdown">
+     <button class="dropbtn">Organizations
+     <i class="fa fa-caret-down"></i>
+     </button>
+   <div class="dropdown-content">
+     <a href="#">Link 1</a>
+     <a href="#">Link 2</a>
+     <a href="#">Link 3</a>
+   </div>
+   </div>
+   <div class="dropdown">
+     <button class="dropbtn">Job Corner
+     <i class="fa fa-caret-down"></i>
+     </button>
+   <div class="dropdown-content">
+     <a href="#">Link 1</a>
+     <a href="#">Link 2</a>
+     <a href="#">Link 3</a>
+   </div>
+   </div>
+   <a href="career.php">Career Guidance</a>
+   <a href="gallery.php">Gallery</a>
+   <a href="#notice">Notice</a>
+   <a href="contact.php">Contact us</a>
+   <div class="log">
+           <a href="registertis.php">Register</a>
+           <a href="logint.html">Login</a>
+   </div>
+  </div>
+
+  <div class="navh">
+    North East University Bangladesh Alumni Association >>> Home
+  </div>
+
+    <div id="banner">
+      <a href="index.html">Find Out More</a>
+    </div>
+
+
+  <?php
+  get_footer();
+  ?>
