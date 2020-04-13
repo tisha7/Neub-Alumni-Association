1452780805eca7b71ad16be969a1da8177744e0e
Ishrat Jahan Tisha <47575896+tisha7@users.noreply.github.com>
2020-04-14 03:00:33 +0600

    Delete breadcrumb.php

--- admin/inc-out/breadcrumb.php
+++
@@ -1,9 +0,0 @@
-        <div id="content" class="span10">
-			    <ul class="breadcrumb">
-				<li>
-					<i class="icon-home"></i>
-					<a href="index.html">Admin Panel</a> 
-					<i class="icon-angle-right"></i>
-				</li>
-				<li><a href="#">Dashboard</a></li>
-			    </ul>
